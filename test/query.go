8648cee94e6aed19a168ec5f8a4f59134d2058c6
Adam Bliss <abliss@gmail.com>
2013-11-27 11:31:37 +0000

    use channels

--- test/query.go
+++ test/query.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/syndtr/goleveldb/leveldb"
+	"os"
 )
 
 type Fact struct {
@@ -35,19 +36,9 @@ type Fact struct {
 	}
 }
 
-func main() {
-	db, err := leveldb.OpenFile("out-v2/facts.leveldb", nil)
-	if err != nil {
-		fmt.Print(err)
-		panic(-1)
-	}
-	defer db.Close()
+func get_facts_by_prefix(db *leveldb.DB, start []byte, out chan<- Fact) {
 	iter := db.NewIterator(nil)
-	//start := []byte("[[[0,[1,T0.0],[0,[2,T0.0,[3,T0.1,T0.2]],[4,[2,T0.0,T0.1],[2,T0.0,T0.2]]]],[],[]],")
-	start := []byte("[[[0,[1,T0.0],[0,[2,T0.0,[")
-
 	end := append(start, byte(0xff))
-
 	iter.Seek(start)
 	for {
 		key := iter.Key()
@@ -55,21 +46,40 @@ func main() {
 			break
 		}
 		value := iter.Value()
-		fmt.Printf("Key: %s\n", key)
 		var fact Fact
 		err := json.Unmarshal(value, &fact)
 		if err != nil {
-			fmt.Printf("Error: %v\nValue: %s\n", err, value)
+			fmt.Fprintf(os.Stderr, "Error: %v\nValue: %s\n", err, value)
 			panic(-1)
 		} else {
-			fmt.Printf("Name: %v\n", fact.Skin.Name)
-			fmt.Printf("Stmt: %v\n", fact.Bone.Stmt)
-			//fmt.Printf("End?: %v\n",
-			fmt.Println()
+			out <- fact
 		}
 		if !iter.Next() {
 			break
 		}
 	}
+	close(out)
 	iter.Release()
+}
+
+func main() {
+	db, err := leveldb.OpenFile("out-v2/facts.leveldb", nil)
+	if err != nil {
+		fmt.Print(err)
+		panic(-1)
+	}
+	defer db.Close()
+	start := []byte("[[[0,[1,T0.0],[0,[2,T0.0,[3,")
+	facts := make(chan Fact)
+	go get_facts_by_prefix(db, start, facts)
+	for {
+		fact, ok := <-facts
+		if !ok {
+			break
+		}
+		fmt.Printf("Name: %v\n", fact.Skin.Name)
+		fmt.Printf("Stmt: %v\n", fact.Bone.Stmt)
+		fmt.Println()
+	}
+
 }
